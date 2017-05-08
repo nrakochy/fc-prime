5656a1e07dffd458809092c27e4d356dc641b714
Nick Rakochy <nick.rakochy@gmail.com>
2017-05-08 12:07:12 -0500

    Added lib api.

--- src/fc_prime/core.clj
+++ src/fc_prime/core.clj
@@ -4,3 +4,12 @@
   "Finds if number is multiple of given prime" 
   [prime-num param]
    (= (mod param prime-num) 0))
+
+(defn prime-sieve [num-primes coll]
+  ())
+
+(defn find-primes 
+  "Finds desired number primes between 2 and a given max"
+  ([] (find-primes 1000))
+  ([range-max] (find-primes range-max 10))
+  ([range-max num-primes] (prime-sieve (range 2 range-max))))
