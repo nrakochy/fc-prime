761c229510f1f6f151b27c33f6baa8a9258775c0
Nick Rakochy <nick.rakochy@gmail.com>
2017-05-08 09:39:39 -0500

    is-multiple? correctly determines if a given number is a multiple of a given prime

--- src/fc_prime/core.clj
+++ src/fc_prime/core.clj
@@ -1,6 +1,6 @@
 (ns fc-prime.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn is-multiple? 
+  "Finds if number is multiple of given prime" 
+  [prime-num param]
+   (= (mod param prime-num) 0))
--- test/fc_prime/core_test.clj
+++ test/fc_prime/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [fc-prime.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def first-ten-primes [2 3 5 7 11 13 17 19 23 29])
+
+(deftest is-multiple-test
+  (let [prime (first first-ten-primes)
+        multiple (* prime 2)
+        prime-as-param (last first-ten-primes)]
+    (testing "returns true when param is a multiple of given prime"
+      (is (= true (is-multiple? prime multiple))))
+    (testing "returns true when param is equal to given prime" 
+      (is (= true (is-multiple? prime prime))))
+    (testing "returns false when param is not multiple of given prime"
+      (is (= false (is-multiple? prime prime-as-param))))))
+
+
+
