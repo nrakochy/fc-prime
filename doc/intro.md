01d302fbc1f955ec3d7b391fd57a950fb7538f17
Nick Rakochy <nick.rakochy@gmail.com>
2017-05-08 09:13:55 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fc-prime
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject fc-prime "0.1.0-SNAPSHOT"
+  :description "Prime number extravaganza"
+  :url "https://github.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]]}})
