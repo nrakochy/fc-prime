43618c418d037d5fef98c1438022866b1f29e9ba
Nick Rakochy <nick.rakochy@gmail.com>
2017-05-08 14:07:23 -0500

    Switched is-multiple? to is-not-multiple?. Extracted recursion termination into stand-alone method

--- src/fc_prime/core.clj
+++ src/fc_prime/core.clj
@@ -1,15 +1,26 @@
 (ns fc-prime.core)
 
-(defn is-multiple? 
-  "Finds if number is multiple of given prime" 
-  [prime-num param]
-   (= (mod param prime-num) 0))
+(defn is-not-multiple? 
+  "Finds if number is not a multiple of given prime" 
+  [param prime-num]
+   (not= (mod param prime-num) 0))
+
+(defn filter-multiples [reduced-coll]
+  (filter #(is-not-multiple? % (first reduced-coll)) (rest reduced-coll)))
+
+(defn terminate-prime-finder? [num-primes results coll]
+  (or (= (count results) num-primes) (empty? coll)))
 
 (defn prime-sieve [num-primes coll]
-  ())
+    (loop [num-primes num-primes
+           results []
+           reduced-coll coll]
+      (if (terminate-prime-finder? results num-primes reduced-coll) 
+        results
+        (recur num-primes (conj results (first reduced-coll)) (filter-multiples reduced-coll)))))
 
 (defn find-primes 
   "Finds desired number primes between 2 and a given max"
   ([] (find-primes 1000))
   ([range-max] (find-primes range-max 10))
-  ([range-max num-primes] (prime-sieve (range 2 range-max))))
+  ([range-max num-primes] (prime-sieve num-primes (range 2 range-max))))
--- test/fc_prime/core_test.clj
+++ test/fc_prime/core_test.clj
@@ -3,17 +3,37 @@
             [fc-prime.core :refer :all]))
 
 (def first-ten-primes [2 3 5 7 11 13 17 19 23 29])
+(def multiples-five [5 10 15 20 25]) 
 
-(deftest is-multiple-test
+(deftest is-not-multiple?-test
   (let [prime (first first-ten-primes)
         multiple (* prime 2)
         prime-as-param (last first-ten-primes)]
-    (testing "returns true when param is a multiple of given prime"
-      (is (= true (is-multiple? prime multiple))))
-    (testing "returns true when param is equal to given prime" 
-      (is (= true (is-multiple? prime prime))))
-    (testing "returns false when param is not multiple of given prime"
-      (is (= false (is-multiple? prime prime-as-param))))))
+    (testing "returns false when param is a multiple of given prime"
+      (is (= false (is-not-multiple? multiple prime))))
+    (testing "returns false when param is equal to given prime" 
+      (is (= false (is-not-multiple? prime prime))))
+    (testing "returns true when param is not multiple of given prime"
+      (is (= true (is-not-multiple? prime-as-param prime))))))
 
+(deftest filter-multiples-test
+  (testing "filters and returns empty sequence where all numbers in collection are multiples of given prime"
+    (let [expected-result (sequence '())]
+      (is (= expected-result (filter-multiples multiples-five))))
+  (testing "filters and returns sequable collection where num is not a multiple of given prime"
+    (let [expected-result (rest first-ten-primes)]
+      (is (= expected-result (filter-multiples first-ten-primes)))))))
 
-
+(deftest terminate-prime-finder?-test 
+  (testing "returns true if results size is equal to requested number of primes"
+    (let [requested-num 2
+          two-primes (take requested-num first-ten-primes)]
+      (is (= true (terminate-prime-finder? requested-num two-primes (range 2))))))
+  (testing "returns true if reduced collection is empty" 
+    (let [requested-num 1000
+          empty-seq (sequence '())]
+      (is (= true (terminate-prime-finder? requested-num first-ten-primes empty-seq)))))
+  (testing "returns false if requested num of primes has not been reached and there are still numbers in the
+           reduced collection param"
+    (let [requested-num 1000]
+            (is (= false (terminate-prime-finder? requested-num [] first-ten-primes))))))
